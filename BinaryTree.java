5081ecfa57ff06a89c24eab83bac9dbe50cd3fa6
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-01-13 15:08:19 +0530

    leetcode 102

--- BinaryTree.java
+++ BinaryTree.java
@@ -55,4 +55,31 @@ class Solution {
     }
 }
 
+//Leetcode 102
+class Solution {
+    private int levels(TreeNode root)
+    {
+        if(root==null) return 0;
+        return 1+Math.max(levels(root.right),levels(root.left));
+    }
+    private void nThLevel(TreeNode root,int level,int lvl,List<Integer> arr)
+    {
+        if(root==null) return;
+        if(level==lvl) arr.add(root.val);
+        nThLevel(root.left,level+1,lvl,arr);
+        nThLevel(root.right,level+1,lvl,arr);
+        
+    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+         List<List<Integer>> ans = new ArrayList<>();
+         int lvl=levels(root);
+         for(int i=0;i<lvl;i++)
+         {
+            List<Integer> arr= new ArrayList<>();
+            nThLevel(root,0,i,arr);
+            ans.add(arr);
+         }
+         return ans;
+    }
+}
 
