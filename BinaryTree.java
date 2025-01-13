0d60b0ce96c4d23ca2eca752e69a6fb418ba5abc
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-01-13 14:16:13 +0530

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -20,3 +20,26 @@ class Solution
       return ans;
     }
   }
+
+// Leetcode 236
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+        return (left != null) ? left : right;
+    }
+}
+
+
