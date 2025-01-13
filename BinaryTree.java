ce09b14549bc74d55ec146bc390249e28077cea0
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-01-13 14:26:19 +0530

    Leetocode questions

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,3 +1,16 @@
+//Leetcode 100
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null) return true;
+        if(p==null && q!=null) return false;
+        if(p!=null && q==null) return false;
+        if(p.val!=q.val) return false;
+        
+        if(isSameTree(p.left,q.left)==false) return false;
+        if(isSameTree(p.right,q.right)==false) return false;
+        return true;
+    }
+}
 // Leetcode 257
 class Solution
   {
