2d20a9e761a9a596d243eb74047c4d0660560905
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-01-16 16:05:39 +0530

    Update Binary Search Tree.java

--- Binary Search Tree.java
+++ Binary Search Tree.java
@@ -45,3 +45,87 @@ class Solution {
         return root;
     }
 }
+
+//Leetcode 94
+class Solution
+{
+    static List<Integer> ans;
+    public void inOrder(TreeNode root)
+    {
+        if(root==null) return;
+        inOrder(root.left);
+        ans.add(root.val);
+        inOrder(root.right);
+    }
+    public List<Integer> inorderTraversal(TreeNode root)
+    {
+        ans= new ArrayList<>();
+        inOrder(root);
+        return ans;
+    }
+}
+
+//method 2
+class Solution {
+    static List<Integer> ans;
+    public static void inOrder(TreeNode root)
+    {
+        TreeNode curr=root
+        ;
+        while(curr!=null)
+        {
+            //no left child
+            if(curr.left==null)
+            {
+                ans.add(curr.val);
+                curr=curr.right;
+            }
+            //there is a left child
+            else
+            {
+                //rightmost child to the left subtree
+                TreeNode prev= curr.left;
+                while(prev.right!=null && prev.right!=curr)
+                {
+                    prev=prev.right;
+                }
+                //case 2: when rightmost child is null
+                if(prev.right==null)
+                {
+                    prev.right=curr;
+                    curr=curr.left;
+                }
+                //not null
+                else
+                {
+                    prev.right=null;
+                    ans.add(curr.val);
+                    curr=curr.right;
+                }
+            }
+        }
+        
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        ans = new ArrayList<>();
+        inOrder(root);
+        return ans;
+    }
+}
+
+//Leetcode 230
+class Solution {
+    static List<Integer> ans;
+    public void inorder(TreeNode root)
+    {
+        if(root==null) return;
+        inorder(root.left);
+        ans.add(root.val);
+        inorder(root.right);
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        ans = new ArrayList<>();
+        inorder(root);
+        return ans.get(k-1);
+    }
+}
