c9e8cc23380af03182972123c6aba26ab59da854
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-01-17 15:15:19 +0530

    Update Binary Search Tree.java

--- Binary Search Tree.java
+++ Binary Search Tree.java
@@ -359,3 +359,64 @@ class Solution {
         return helper(preorder,inorder,0,n-1,0,n-1);
     }
 }
+
+//Leetcode 450
+ */
+class Solution {
+    public TreeNode iop(TreeNode root)
+    {
+        TreeNode temp = root.left;
+        while(temp.right!=null) temp = temp.right;
+        return temp;
+    }
+    public TreeNode parent(TreeNode root, TreeNode pred)
+    {
+        if(root.left==pred || root.right==pred) return root;
+        TreeNode temp = root.left;
+        while(temp.right!=pred) temp =temp.right;
+        return temp;
+    }
+    
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null) return null;
+        if(root.val==key)
+        {
+            //case 1: leaf node:0 child
+            if(root.left == null && root.right==null)
+            {return null;}
+
+            //case 2:1 child node
+            else if(root.left==null || root.right==null)
+            {
+                if(root.left==null)return root.right;
+                else return root.left;
+            }
+            //case 3:  2 child nodes
+            else
+            {
+                TreeNode pred= iop(root);
+                TreeNode predParent= parent(root,pred);
+                // note that if the key has 2 children then we will follow the predecessor approach
+                if(root==predParent)
+                
+                {
+                    pred.right=root.right;
+                    return pred;
+                }
+                predParent.right= pred.left;
+                pred.left = root.left;
+                pred.right = root.right;
+                return pred;
+            }
+        }
+        else if(root.val>key)
+        {
+            root.left = deleteNode(root.left,key);
+        }
+        else if(root.val<key)
+        {
+            root.right = deleteNode(root.right,key);
+        }
+        return root;
+    }
+}
