3d967ef28ea58d6f498284b995fa9dbbe2fe7df0
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-01-20 17:51:09 +0530

    Update HashMap and HashSet.java

--- HashMap and HashSet.java
+++ HashMap and HashSet.java
@@ -166,4 +166,37 @@ class Solution {
     }
 }
 
+//Leetcode 1497
+import java.util.HashMap;
+
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int ele = ((arr[i] % k) + k) % k; // Ensure non-negative remainder
+            map.put(ele, map.getOrDefault(ele, 0) + 1);
+        }
+
+        for (int key : map.keySet()) {
+            if (key == 0) { 
+
+                if (map.get(key) % 2 != 0) return false;
+            } else {
+                int rem = k - key;
+                if (key == rem) { 
+          
+                    if (map.get(key) % 2 != 0) return false;
+                } else {
+                    if (!map.containsKey(rem) || !map.get(key).equals(map.get(rem))) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
+
+
 
