9e63fd7e644590b1f10cadcce7f6f094e56e98bb
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-01-20 17:49:42 +0530

    Update HashMap and HashSet.java

--- HashMap and HashSet.java
+++ HashMap and HashSet.java
@@ -140,3 +140,30 @@ class Solution {
     }
 }
 
+//Leetcode 3
+import java.util.HashMap;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int maxlen = 0;
+        int n = s.length();
+        if (n <= 1) return n;
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        int i = 0, j = 0;
+        
+        while (j < n) {
+            char ch = s.charAt(j);
+            if (map.containsKey(ch) && map.get(ch) >= i) {
+                i = map.get(ch) + 1;
+            }
+            map.put(ch, j);
+            maxlen = Math.max(maxlen, j - i + 1);
+            j++;
+        }
+        
+        return maxlen;
+    }
+}
+
+
