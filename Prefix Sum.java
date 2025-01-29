0e26c95aec461d9fccf56316396b2a8d1bb60159
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-01-29 14:19:47 +0530

    Update Prefix Sum.java

--- Prefix Sum.java
+++ Prefix Sum.java
@@ -33,3 +33,69 @@ int[]arr;
  * NumArray obj = new NumArray(nums);
  * int param_1 = obj.sumRange(left,right);
  */
+
+//Leetcode 724
+// class Solution {
+//     public int pivotIndex(int[] nums) {
+//         int n=nums.length;
+//         for(int i=0;i<n;i++)
+//         {
+//             int leftSum=0, rightSum=0;
+//             for(int j=0;j<i;j++)
+//             {
+//                 leftSum += nums[j];
+//             }
+//             for(int j=i+1;j<n;j++)
+//             {
+//                 rightSum+=nums[j];
+//             }
+//             if(leftSum == rightSum) return i;
+
+//         }
+//         return -1;
+//     }
+// }
+// TC:O(n2^) methpd is brutforce
+
+class Solution {// method 2:prefix sum  //TC:O(n)
+//extra space O(1)
+    public int pivotIndex(int[] nums) {
+        int n= nums.length;
+        //th better approach is prefix sum 
+        for(int i=1;i<n;i++)
+        {
+            nums[i]+=nums[i-1];
+        }
+        for(int i=0;i<n;i++)
+        {
+            int leftSum=0;
+            if(i>0) leftSum= nums[i-1];
+            int rightSum = nums[n-1]- nums[i];
+            if(leftSum==rightSum) return i;
+        }
+        return -1;
+
+    }}
+
+//Leetcode 2640
+class Solution {
+    public long max(long i,long j)
+    {
+        return Math.max(i,j);
+    }
+    public long[] findPrefixScore(int[] nums) {
+        int n=nums.length;
+        long[] arr=new long[n];
+        long maxVal=0;
+        for(int i=0;i<n;i++)
+        {
+            maxVal=max(maxVal,nums[i]);
+            arr[i]=nums[i]+maxVal;
+        }
+        for(int i=1;i<n;i++)
+        {
+            arr[i]=arr[i]+arr[i-1];
+        }
+        return arr;
+    }
+}
