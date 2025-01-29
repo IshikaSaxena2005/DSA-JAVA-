5541dc56751f6655200fa6032f1777273be73750
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-01-29 14:56:13 +0530

    Update Prefix Sum.java

--- Prefix Sum.java
+++ Prefix Sum.java
@@ -99,3 +99,80 @@ class Solution {
         return arr;
     }
 }
+
+//Leetcode 238
+// //O(n^2) approach
+// class Solution {
+//     public int[] productExceptSelf(int[] nums) {
+//         int n=nums.length;
+//         int [] ans = new int[n];
+//         for(int i=0;i<n;i++)
+//         {
+//             int leftPrd=1,rightPrd=1;
+//             for(int j=0;j<i;j++)
+//             {
+//                 leftPrd *=nums[j];
+//             }
+//             for(int j=i+1;j<n;j++)
+//             {
+//                 rightPrd *= nums[j];
+//             }
+//             ans[i]=leftPrd* rightPrd;
+//         }
+//         return ans;
+//     }
+// }//Giving TLE error
+
+class Solution {
+    public int[] productExceptSelf(int[] arr) {
+        int n=arr.length;
+        //pre[i]=pre[i-1]* arr[i-1]
+        int[]pre=new int[n];
+        pre[0]=1;
+        for(int i=1;i<n;i++)
+        {
+            pre[i]=pre[i-1] * arr[i-1];
+        }
+        int[]suf=new int[n];
+        suf[n-1]=1;
+        for(int i=n-2;i>=0;i--)
+        {
+            suf[i]=arr[i+1]* suf[i+1];
+        }
+        int[]ans = new int[n];
+        for(int i=0;i<n;i++)
+
+        {
+            ans[i]=suf[i]* pre[i];
+        }
+        return ans;
+        
+    }} //TC:O(n)
+
+//Leetcode 2389
+class Solution {
+    public int[] answerQueries(int[] arr, int[] queries) {
+        Arrays.sort(arr);
+        int n= arr.length, m= queries.length;
+        for(int i=1;i<n;i++)
+        {
+            arr[i] +=arr[i-1];
+        }
+        int ans[]= new int[m];
+        for(int i=0;i<m;i++)
+        {
+            int lo=0,hi=n-1;
+            while(lo<=hi)
+            {
+                int mid = lo+(hi-lo)/2;
+                if(arr[mid]>queries[i]) hi= mid-1;
+                else
+                {
+                    ans[i]= Math.max(ans[i],mid+1);
+                    lo = mid+1;
+                }
+            }
+        }
+        return ans;
+    }
+}
