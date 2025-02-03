9aaafc468ac7fa8b7b8bd033c7a762eb485e5628
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-02-03 17:01:10 +0530

    Update Dynamic Programming.java

--- Dynamic Programming.java
+++ Dynamic Programming.java
@@ -392,3 +392,78 @@ class Solution {
         return minSquares(n,dp);
     }
 } //for recursive soln TC is exponential
+
+//FROG JUMP (GFG)
+// Given an integer array height[] where height[i] represents the height of the i-th stair, 
+//     a frog starts from the first stair and wants to reach the top. From any stair i, 
+//     the frog has two options: it can either jump to the (i+1)th stair or the (i+2)th stair. 
+//     The cost of a jump is the absolute difference in height between the two stairs.
+//     Determine the minimum total cost required for the frog to reach the top.
+
+class Solution {
+    int minCost(int[] height) {
+        // code here
+        int n=height.length;
+        if(n==1) return 0;
+        int prev2=0;
+        int prev1=Math.abs(height[0]-height[1]);
+        for(int i=2;i<n;i++)
+        {
+            int curr=Math.min(prev1+Math.abs(height[i]-height[i-1]), prev2+Math.abs(height[i]-height[i-2]));
+            prev2= prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+}
+
+// Given an array arr[] of size n,
+//     where arr[i] denotes the height of ith stone. Geek starts from stone 0 and from stone i,
+//     he can jump to stones i + 1, i + 2, … i + k. The cost for jumping from stone i to stone j 
+//     is abs(arr[i] – arr[j]). Find the minimum cost for Geek to reach the last stone.
+
+class Solution {
+    public int minimizeCost(int k, int arr[]) {
+        // code here
+        int n=arr.length;
+        int[]dp= new int[n];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
+        
+        for(int i=1;i<n;i++)
+        {
+            for(int j=Math.max(0,i-k);j<i;j++)
+            {
+                dp[i]=Math.min(dp[i],dp[j]+Math.abs(arr[i]-arr[j]));
+            }
+        }
+        return dp[n-1];
+    }
+}
+
+//Leetcode 213
+class Solution {
+    public int robLinear(int[]nums,int start,int end)
+    {
+        int n=end-start+1;
+        if(n==1) return nums[start];
+        int []dp= new int[n];
+        dp[0]=nums[start];
+        dp[1]=Math.max(nums[start],nums[start+1]);
+        for(int i=2;i<n;i++)
+        {
+            dp[i]=Math.max(nums[start+i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
+    }
+    public int rob(int[] nums) {
+        //since houses are arranged in a circle, we cannot rob both first and last houses
+        //split problem into 2 cases
+        //rob house from 0 to n-2
+        //rob house from 1 to n-1
+    int n=nums.length;
+    if(n==1) return nums[0];
+    return Math.max(robLinear(nums,0,n-2),robLinear(nums,1,n-1));
+
+    }
+}
