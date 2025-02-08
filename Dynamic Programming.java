b06cb580ecec8e84e5aefd1534522f62bcdb7694
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-02-08 12:08:49 +0530

    Update Dynamic Programming.java

--- Dynamic Programming.java
+++ Dynamic Programming.java
@@ -716,3 +716,54 @@ class Solution {
          return dp[m-1][n-1];
      }
 }
+
+//Leetcode 322
+//using recursion
+class Solution {
+    public long countcoin(int i,int[]coins,int amount)
+    {
+        if(i==coins.length){
+             if(amount==0) return 0; //no more coins needed
+        else return Integer.MAX_VALUE; //not a valid combination
+        }
+       
+        long skip=countcoin(i+1,coins,amount);
+        if(amount-coins[i]<0) return skip;
+        long pick=1+countcoin(i,coins,amount-coins[i]);
+        return Math.min(skip,pick);
+    }
+    public int coinChange(int[] coins, int amount) {
+        int ans=(int)countcoin(0,coins,amount);
+        if(ans==Integer.MAX_VALUE) return -1;
+        return ans;
+    }
+}
+
+//using DP
+class Solution {
+    public long countcoin(int i,int[]coins,int amount,long[][]dp)
+    {
+        if(i==coins.length){
+             if(amount==0) return 0; //no more coins needed
+        else return Integer.MAX_VALUE; //not a valid combination
+        }
+       if(dp[i][amount]!=-1) return dp[i][amount];
+        long skip=countcoin(i+1,coins,amount,dp);
+        if(amount-coins[i]<0) return skip;
+        long pick=1+countcoin(i,coins,amount-coins[i],dp);
+        return dp[i][amount]=Math.min(skip,pick);
+    }
+    public int coinChange(int[] coins, int amount) {
+        long[][] dp= new long[coins.length][amount+1];
+        for(int i=0;i<dp.length;i++)
+        {
+            for(int j=0;j<dp[0].length;j++)
+            {
+                dp[i][j]=-1;
+            }
+        }
+        int ans=(int)countcoin(0,coins,amount,dp);
+        if(ans==Integer.MAX_VALUE) return -1;
+        return ans;
+    }
+}
