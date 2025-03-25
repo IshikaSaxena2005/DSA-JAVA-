671d91e75507436c213c0160099c7bcf9bc64b28
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-03-25 22:05:45 +0530

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -22,7 +22,30 @@ class Solution {
     }
 }
 
-
+public int[]remove(int arr[])
+    {
+        Stack<Integer>st= new Stack<>();
+        for(int i=0;i<arr.length;i++)
+            {
+                if(st.isEmpty()) st.push(arr[i]);
+                else if(st.peek()==arr[i]) st.push(arr[i]);
+                else if(st.peek()==arr[i])
+                {
+                    if(arr[i]==n-1 || arr[i]!=arr[i+1])
+                    {
+                        st.pop();
+                    }
+                    else 
+                        continue;
+                }
+            }
+            for(int i=arr.length-1;i>0;i--)
+                {
+                    arr[i]=st.pop();
+                }
+    return arr;
+    
+    }
 
 
 
