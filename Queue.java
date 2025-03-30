e2d250ce15c3d97a6ac4f94a4d224504907ba7e2
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-03-30 12:12:24 +0530

    stack and queue implementaion

--- Queue.java
+++ Queue.java
@@ -95,7 +95,56 @@ class MyStack {
  * boolean param_4 = obj.empty();
  */
 
+//Leetcode 232
+class MyQueue {
+    Stack<Integer> input=new Stack<>();
+    Stack<Integer> output= new Stack<>();
+    public MyQueue() {
+        
+    }
+    
+    public void push(int x) {
+        input.push(x);
+    }
+    
+    public int pop() {
+        if(output.empty())
+        
+            while(input.empty()==false)
+            {
+                output.push(input.peek());
+                input.pop();
+            }
+            int x=output.peek();
+            output.pop();
+                 return x;
+        
+   
+    }
+    
+    public int peek() {
+        if(output.empty())
+        while(input.empty()==false)
+        {
+            output.push(input.peek());
+            input.pop();
+        }
+        return output.peek();
+    }
+    
+    public boolean empty() {
+       return input.isEmpty() && output.isEmpty();
+    }
+}
 
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
 
 
 
@@ -106,3 +155,4 @@ class MyStack {
 
 
 //Leetcode 225
+//Leetcode 232
