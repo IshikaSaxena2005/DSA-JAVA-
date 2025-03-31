89dbc4a2b9af619fb69b8bc5d4ea5b8565942d07
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-03-31 11:26:22 +0530

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -236,8 +236,79 @@ class Solution {
     }
 }
 
+//Leetcode 402
+class Solution {
+    public String removeKdigits(String num, int k) {
+        Stack<Character> st= new Stack<>();
+        for(int i=0;i<num.length();i++)
+        {
+            char c = num.charAt(i);
+            while(!st.isEmpty()&& k>0 && st.peek()>c)
+            {
+                st.pop();
+                k--;
+            }
+            st.push(c);
+        }
+        //still  kuch ele reh gye
+        while(!st.isEmpty()&& k>0)
+        {
+            st.pop();
+            k--;
+        }
+        StringBuilder res= new StringBuilder();
+        while(!st.isEmpty())
+        {
+            res.append(st.peek());
+            st.pop();
+        }
+        res.reverse();
+        //handling zeros in starting
+        while(res.length()>0 && res.charAt(0)=='0')
+        {
+            res.deleteCharAt(0);
+        }
+         return res.length()==0?"0":res.toString();
+
+    }
+}
+
+//Leetcode 42
+class Solution {
+    public int trap(int[] height) {
+        int n=height.length;
+        if(n==0) return 0;
+        int waterTrapped=0;
+        int[] leftMax=new int[n];
+        int[] rightMax=new int[n];
+        leftMax[0]=height[0];
+        for(int i=1;i<n;i++)
+        {
+           leftMax[i]=Math.max(leftMax[i-1],height[i]);
+            
+        }
+        rightMax[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            rightMax[i]=Math.max(rightMax[i+1],height[i]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            waterTrapped+=Math.min(leftMax[i],rightMax[i])-height[i];
+        }
+        return waterTrapped;
+    }
+}
+
+
+
+
+
+
 //Leetcode 225
 //Leetcode 232
 //First negative in each window size k
 //Reorder Queue
 //Leetcode 735
+//Leetcode 402
+//Leetcode 42
