6925434b749f87c018db47d312239a56db7bc287
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-04-02 07:35:01 +0530

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,3 +1,38 @@
+//Leetcode 226
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invert(TreeNode root)
+    {
+        if(root==null) return null;
+        if(root.left==null &&root.right==null) return null;
+        TreeNode temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+        invert(root.left);
+        invert(root.right);
+        return root;
+    }
+    public TreeNode invertTree(TreeNode root) {
+        invert(root);
+        return root;
+        
+    }
+}
+
 //Leetcode 100
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
