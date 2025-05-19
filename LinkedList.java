d611316b7005915f216e46d1ac8958b2bc44dba1
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-05-19 10:05:06 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -175,3 +175,40 @@ class Solution {
         return newHead;
     }
 }
+
+//MERGE 2 sorted LL
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(100);
+        ListNode temp=dummy;
+        ListNode temp1=list1;
+        ListNode temp2=list2;
+        while(temp1!=null && temp2!=null)
+        {
+            if(temp1.val<=temp2.val)
+            {
+                temp.next=temp1;
+                temp1=temp1.next;
+            }
+            else
+            {
+                temp.next=temp2;
+                temp2=temp2.next;
+            }
+            temp=temp.next;
+        }
+        if(temp1==null) temp.next=temp2;
+        else temp.next=temp1;
+        return dummy.next;
+    }
+}
