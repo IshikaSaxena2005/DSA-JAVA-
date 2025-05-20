2e7b60a268c19fa76800813810ff4a4f3d4631ca
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-05-20 09:34:05 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -212,3 +212,53 @@ class Solution {
         return dummy.next;
     }
 }
+
+
+//Sort List
+class Solution {
+    public ListNode merge(ListNode temp1,ListNode temp2)
+    {
+        ListNode dummy= new ListNode(100);
+        ListNode temp=dummy;
+        while(temp1!=null & temp2!=null)
+        {
+
+        if(temp1.val<=temp2.val)
+        {
+            temp.next=temp1;
+            temp1=temp1.next;
+        }
+        else
+        {
+            temp.next=temp2;
+            temp2=temp2.next;
+        }
+        temp=temp.next;
+          
+        }
+        if(temp1==null) temp.next=temp2;
+        else temp.next=temp1;
+        return dummy.next;
+
+    }
+    public ListNode sortList(ListNode head) {
+        if(head==null ||head.next==null) return head;
+        ListNode fast=head;
+        ListNode slow=head;
+        ListNode firstHalf=head;
+        while(fast.next!=null && fast.next.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode secondHalf=slow.next;
+        slow.next=null;
+        firstHalf=sortList(firstHalf);
+        secondHalf=sortList(secondHalf);
+        ListNode ans= merge(firstHalf,secondHalf);
+        return ans;
+    }
+}
+
+//Leetcode 21
+//Leetocode 148
