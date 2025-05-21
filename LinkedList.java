c4fa06324cf146c21892a72e99d8cb761a91c8f1
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-05-21 10:48:35 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -470,6 +470,50 @@ class Solution {
 
     }
 }   
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverse(ListNode head)
+    {
+        ListNode curr=head;
+        ListNode prev=null;
+        ListNode next=null;
+        while(curr!=null)
+        {
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+      public boolean isPalindrome(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null )
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode j=reverse(slow);
+        ListNode i=head;
+        while(j!=null)
+        {
+            if(i.val!=j.val) return false;
+            i=i.next;
+            j=j.next;
+        }
+        return true;
+      }
 //Leetcode 21
 //Leetocode 148
 //Leetcode 23
