ecae9ab99cce102f098373d94ba4f03745d587fd
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-05-22 09:12:35 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -561,6 +561,49 @@ class Solution {
     }
 }
 
+//add 2 numbers
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy= new ListNode(100);
+        ListNode temp=dummy;
+        int carry=0;
+        while(l1!=null || l2!=null)
+        {
+            int val1=0;
+            int val2=0;
+            if(l1!=null) val1=l1.val;
+            if(l2!=null) val2=l2.val;
+            int num=val1+val2+carry;
+            ListNode node= new ListNode(num%10);
+            temp.next=node;
+            temp=temp.next;
+            if(num>9) carry=1;
+            else carry=0;
+            if(l1!=null)l1=l1.next;
+            if(l2!=null)l2=l2.next;
+        }
+        if(carry==1) 
+        {
+            ListNode node= new ListNode(1);
+            temp.next=node;
+            temp=temp.next;
+
+
+        }
+        return dummy.next;
+    }
+}
+
     
 //Leetcode 21    
 //Leetocode 148
@@ -571,3 +614,4 @@ class Solution {
 //Leetcode 24
 //Leetcode 234
 //Leetcode 92
+//Leetcode 2
