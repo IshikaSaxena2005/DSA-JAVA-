c3cc8b75aaf7bb64b39caade003d96503eea437b
Ishika Saxena <115330322+IshikaSaxena2005@users.noreply.github.com>
2025-06-18 15:48:18 +0530

    Update Array_ArrayList.java

--- Array_ArrayList.java
+++ Array_ArrayList.java
@@ -231,7 +231,44 @@ class Solution {
 
 }
 
-
+//merge 2 sorted arrays
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[]ans=new int[m+n];
+        int k=0;
+        int i=0;
+        int j=0;
+        while(i<m &&j<n)
+        {
+            if(nums1[i]<nums2[j])
+            {
+                ans[k]=nums1[i];
+                 i++;
+                k++;
+            }
+            else 
+            {
+                ans[k]=nums2[j];
+                j++;
+                k++;
+            }
+        }
+        while(i<m)
+        {
+            ans[k]=nums1[i];
+            i++;
+            k++;
+        }
+        while(j<n)
+        {
+            ans[k]=nums2[j];
+            j++;
+            k++;
+        }
+          for (int p = 0; p < m + n; p++) {
+            nums1[p] = ans[p];}
+    }
+}
 
 
 
